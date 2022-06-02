89a561ab0f9122487062d998f7fbcbfe01c7ea97
ZAKHAR <37652@studentwsei.pl>
2022-06-02 21:55:54 +0200

    Dodalem pole textField w ktory wstawilem licznik klikniec.

--- src/main/java/com/example/HelloController.java
+++ src/main/java/com/example/HelloController.java
@@ -4,14 +4,19 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 
 public class HelloController {
     @FXML
     public Button btnMain;
     int clickCounter = 0;
+    @FXML
+    public TextField txtLicznik;
 
     @FXML
     public void onBtnAction(ActionEvent actionEvent) {
-        System.out.println("Klikniecie nr " + ++clickCounter);
+        txtLicznik.setText("Klikniecie nr " + ++clickCounter);
+        if(clickCounter == 1)
+            btnMain.setText("I jeszcze raz");
     }
 }
